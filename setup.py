8dbe0924e1981b960270705b1a1147179aa65ac1
Filipi565 <filipi565@hotmail.com>
2025-02-07 11:43:27 -0300

    Add setup files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Extension
+import os
+
+HERE = os.path.abspath(os.path.join(__file__, ".."))
+
+@(lambda f: f())
+def audio_ext():
+    path = os.path.join(HERE, "audio")
+    src = []
+
+    with os.scandir(path) as scandir:
+        for item in scandir:
+            if (item.name.endswith(".c")):
+                src.append(item.path)
+
+    return Extension("_audio", src, include_dirs=[os.path.join(HERE, "include")])
+
+setup(
+    ext_modules=[audio_ext]
+)
