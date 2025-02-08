85a10b6d01f667f2ebe709e4b97fe14fbb39b034
Filipi565 <filipi565@hotmail.com>
2025-02-08 16:28:41 -0300

    Document the code

--- src/audio.py
+++ src/audio.py
@@ -8,15 +8,48 @@ from _audio import ( # type: ignore
 MiniAudioError.__module__ = __name__
 
 def get_devices():
+    """
+    Retrieve a list of available audio devices.
+
+    This function uses the internal `_get_devices` function from the `_audio` module to fetch the list of audio devices.
+    It filters out any None values from the returned list and returns the filtered list.
+
+    Parameters:
+    None
+
+    Returns:
+    list: A list of `DeviceInfo` objects representing the available audio devices.
+    """
     return list(
         filter(lambda obj: obj is not None, _audio._get_devices())
     )
 
 class _AudioDeviceHelper(_audio._AudioDeviceHelper):
     def init(self, audiodevice = None):
+        """
+        Initialize the audio device helper with the given audio device.
+
+        This method initializes the internal state of the audio device helper using the provided audio device.
+        If no audio device is specified, the default audio device will be used.
+
+        Parameters:
+        audiodevice (DeviceId, optional): The identifier of the audio device to be used. If not provided, the default audio device will be used.
+
+        Returns:
+        None
+        """
         super().init(audiodevice)
 
     def close(self):
+        """
+        Close the audio device helper.
+
+        This method closes the audio device helper and releases any associated resources.
+        After calling this method, the audio device helper should not be used anymore.
+
+        Returns:
+        None
+        """
         super().close()
 
     @property
--- src/audio.pyi
+++ src/audio.pyi
@@ -5,9 +5,20 @@ class DeviceId: ...
 
 class DeviceInfo:
     @property
-    def name(self) -> str: ...
+    def name(self) -> str:
+        """
+        Returns the name of the audio device.
+
+        :return: A string representing the name of the audio device.
+        """
+
     @property
-    def id(self) -> DeviceId: ...
+    def id(self) -> DeviceId:
+        """
+        Returns the unique identifier of the audio device.
+
+        :return: An instance of the DeviceId class representing the unique identifier of the audio device.
+        """
 
 class _AudioDeviceHelper:
     def init(self, audiodevice: Union[DeviceId, None] = None) -> None: ...
