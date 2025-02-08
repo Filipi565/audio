f1dd4ebb14ba79a3dd80e8ab189c5493b70784eb
Filipi565 <filipi565@hotmail.com>
2025-02-08 13:04:27 -0300

    Add a simple python audio module

---
+++ src/audio.py
@@ -0,0 +1,26 @@
+import _audio # type: ignore
+from _audio import MiniAudioError # type: ignore
+
+MiniAudioError.__module__ = __name__
+
+class _AudioDeviceHelper(_audio._AudioDeviceHelper):
+    def init(self, audiodevice = None):
+        super().init(audiodevice)
+
+    def close(self):
+        super().close()
+
+    @property
+    def master_volume(self):
+        return super().master_volume(None)
+    
+    @master_volume.setter
+    def master_volume(self, other):
+        if (isinstance(other, int) and not type(other) is bool):
+            other = float(other)
+        super().master_volume(other)
+
+AudioDevice = _AudioDeviceHelper()
+
+del _AudioDeviceHelper
+del _audio
